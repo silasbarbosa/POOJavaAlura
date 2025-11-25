b8641ff2dacee23e95384bc7b4b77562a7147c9c
silasbarbosa <silasbarbosademorais@gmail.com>
2025-11-25 17:11:09 -0300

    atualizacao

---
+++ ProjetoFilmes/src/Series.java
@@ -0,0 +1,35 @@
+public class Series extends Title{
+
+    private int seasons;
+    private int epSeason;
+    private boolean activated;
+    private int minutesEpisode;
+
+
+    public int getSeason() {
+        return seasons;
+    }
+    public void setSeason(int season) {
+        this.seasons = season;
+    }
+    public int getEpSeason() {
+        return epSeason;
+    }
+    public void setEpSeason(int epSeason) {
+        this.epSeason = epSeason;
+    }
+    public boolean isActivated() {
+        return activated;
+    }
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
+    public int getMinutesEpisode() {
+        return minutesEpisode;
+    }
+    public void setMinutesEpisode(int minutesEpisode) {
+        this.minutesEpisode = minutesEpisode;
+    }
+
+
+}
---
+++ ProjetoFilmes/src/Title.java
@@ -0,0 +1,68 @@
+public class Title {
+     private String name;
+    private int year;
+    private boolean includedOnPlan;
+    private double assessment;
+    private int totalRatins;
+    private int durationInMinutes;
+
+    /* get and setters */
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public void setIncludeOnPlan(boolean includedOnPlan){
+        this.includedOnPlan = includedOnPlan;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes){
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isIncludedOnPlan() {
+        return includedOnPlan;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public int getTotalRatins(){
+        return totalRatins;
+    }
+
+    /*Metodo*/
+    public void displaysTechnicalSheet(){
+        System.out.println("Name: " + this.getName());
+        System.out.println("Relase Date: " + this.getYear());
+        System.out.println("Incuded on Plan: " + this.isIncludedOnPlan());
+        System.out.println("Dutation in Minutes: " + this.getDurationInMinutes());
+        System.out.println("Media Assesment: " + this.resultMedia());
+
+    }
+
+    public  void evaluate(double number){
+        this.assessment+= number;
+        totalRatins++;
+    }
+
+    public double resultMedia(){
+        double media = assessment/totalRatins;
+        return media;
+
+    }
+
+
+}
